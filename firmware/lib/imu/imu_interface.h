00b586d3bada5cba737addb88dfec7c21db804e1
Thomas Chou <thomas@wytron.com.tw>
2024-05-13 07:32:34 -0700

    odom imu mag: define covariance to allow customization
    The covariance of various sensors messages is important to the
    navigation stack. This patch allows fine tune the covariance in
    the configuration files for best estimation of robot's pose.
    
    Signed-off-by: Thomas Chou <thomas@wytron.com.tw>

--- firmware/lib/imu/imu_interface.h
+++ firmware/lib/imu/imu_interface.h
@@ -17,6 +17,16 @@
 
 #include <sensor_msgs/msg/imu.h>
 
+#ifndef ACCEL_COV
+#define ACCEL_COV { 0.00001, 0.00001, 0.00001 }
+#endif
+#ifndef GYRO_COV
+#define GYRO_COV { 0.00001, 0.00001, 0.00001 }
+#endif
+#ifndef ORI_COV
+#define ORI_COV { 0.00001, 0.00001, 0.00001 }
+#endif
+
 class IMUInterface
 {
     protected:
@@ -25,8 +35,9 @@ class IMUInterface
         const float mgauss_to_utesla_ = 0.1;
         const float utesla_to_tesla_ = 0.000001;
 
-        float accel_cov_ = 0.00001;
-        float gyro_cov_ = 0.00001;
+        const float accel_cov[3] = ACCEL_COV;
+        const float gyro_cov[3] = GYRO_COV;
+        const float ori_cov[3] = ORI_COV;
         const int sample_size_ = 40;
 
         geometry_msgs__msg__Vector3 gyro_cal_;
@@ -49,7 +60,7 @@ class IMUInterface
             gyro_cal_.y = gyro_cal_.y / (float)sample_size_;
             gyro_cal_.z = gyro_cal_.z / (float)sample_size_;
         }
-   
+
     public:
         IMUInterface()
         {
@@ -73,29 +84,36 @@ class IMUInterface
         {
             imu_msg_.angular_velocity = readGyroscope();
 #ifndef USE_MPU6050_IMU // mpu6050 already calibrated in driver
-            imu_msg_.angular_velocity.x -= gyro_cal_.x; 
-            imu_msg_.angular_velocity.y -= gyro_cal_.y; 
-            imu_msg_.angular_velocity.z -= gyro_cal_.z; 
+            imu_msg_.angular_velocity.x -= gyro_cal_.x;
+            imu_msg_.angular_velocity.y -= gyro_cal_.y;
+            imu_msg_.angular_velocity.z -= gyro_cal_.z;
 #endif
 
             if(imu_msg_.angular_velocity.x > -0.01 && imu_msg_.angular_velocity.x < 0.01 )
-                imu_msg_.angular_velocity.x = 0; 
-         
+                imu_msg_.angular_velocity.x = 0;
+
             if(imu_msg_.angular_velocity.y > -0.01 && imu_msg_.angular_velocity.y < 0.01 )
                 imu_msg_.angular_velocity.y = 0;
 
             if(imu_msg_.angular_velocity.z > -0.01 && imu_msg_.angular_velocity.z < 0.01 )
                 imu_msg_.angular_velocity.z = 0;
-       
-            imu_msg_.angular_velocity_covariance[0] = gyro_cov_;
-            imu_msg_.angular_velocity_covariance[4] = gyro_cov_;
-            imu_msg_.angular_velocity_covariance[8] = gyro_cov_;
-            
-            imu_msg_.linear_acceleration = readAccelerometer();
-            imu_msg_.linear_acceleration_covariance[0] = accel_cov_;
-            imu_msg_.linear_acceleration_covariance[4] = accel_cov_;
-            imu_msg_.linear_acceleration_covariance[8] = accel_cov_;
 
+            imu_msg_.angular_velocity_covariance[0] = gyro_cov[0];
+            imu_msg_.angular_velocity_covariance[4] = gyro_cov[1];
+            imu_msg_.angular_velocity_covariance[8] = gyro_cov[2];
+
+            imu_msg_.linear_acceleration = readAccelerometer();
+            imu_msg_.linear_acceleration_covariance[0] = accel_cov[0];
+            imu_msg_.linear_acceleration_covariance[4] = accel_cov[1];
+            imu_msg_.linear_acceleration_covariance[8] = accel_cov[2];
+
+            imu_msg_.orientation_covariance[0] = ori_cov[0];
+            imu_msg_.orientation_covariance[4] = ori_cov[1];
+            imu_msg_.orientation_covariance[8] = ori_cov[2];
+
+#ifdef IMU_TWEAK
+            IMU_TWEAK
+#endif
             return imu_msg_;
         }
 };
